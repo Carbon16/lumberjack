8ee68ffa9ad158480cfe7c6bd969eac1982b8322
Carbon1769 <92681803+Carbon16@users.noreply.github.com>
2022-11-09 21:02:21 +0000

    ready!

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 "use strict";
 var fs = require('fs');
-const { exit } = require('process');
+
+function writeRiver(riverDir, message) {
+    var path = riverDir +"/river.log"
+    fs.appendFile(path, message, function (err) {
+        if (err) throw err;
+    })
+};
 
 class lumberjack {
     river = new Array()
-    info = new Array()
-    debug = new Array()
-    warn = new Array()
-    error = new Array()
-    critical = new Array()
-    fatal = new Array()
+    infoRiver = new Array()
+    debugRiver = new Array()
+    warnRiver = new Array()
+    errorRiver = new Array()
+    criticalRiver = new Array()
+    fatalRiver = new Array()
+
     constructor(logName = "log", path = null, include = "LOG", verbose = false) {
         if (path == undefined || path == null) {
             throw("You must define the log directory!")
@@ -18,11 +25,12 @@ class lumberjack {
         this.name = logName;
         this.include = include;
         this.verbose = verbose;
-        if (!fs.existsSync(this.dir)) {
+        if (!fs.existsSync((this.dir))) {
             fs.mkdirSync(this.dir, { recursive: true });
         }
-    }
-    log(message, level = "LOG", filename = "", comment = "") {
+    };
+
+    log(message, level = "INFO", filename = "", comment = "") {
         var path = this.dir + "/river.log"
         var dt = new Date().toISOString();
         var entry = {
@@ -32,7 +40,7 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             // console.log(entry)
             // console.log(this.river)
@@ -43,59 +51,35 @@ class lumberjack {
         });
         switch (level) {
             case "INFO":
-                this.info.push(entry);
+                this.infoRiver.push(entry);
                 break;
             case "DEBUG":
-                this.debug.push(entry);
+                this.debugRiver.push(entry);
                 break;
             case "WARN":
-                this.warn.push(entry);
+                this.warnRiver.push(entry);
                 break;
             case "ERROR":
-                this.error.push(entry);
+                this.errorRiver.push(entry);
                 break;
             case "CRITICAL":
-                this.critical.push(entry);
+                this.criticalRiver.push(entry);
                 break;
             case "FATAL":
-                this.fatal.push(entry);
+                this.fatalRiver.push(entry);
                 break;
         };
         this.river.push(entry);
-        if (this.include.includes(level)) {
-            var path = this.dir + "/" + this.name + "/" + level + ".log"
-            fs.appendFile(path, message, function (err) {
-                if (err) throw err;
-            });
-        };
-    };
-    info(message, filename = "", comment = "") {
-        level = "INFO"
-        var dt = new Date().toISOString();
-        var entry = {
-            "level": level,
-            "message": message,
-            "comment": comment,
-            "path": filename,
-            "time": dt
-        };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
-        if (this.verbose) {
-            console.log(message);
-        }
-        this.info.push(entry);
-        this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "INFO.log"
+        //save file
+        var path = this.dir + "/" + level + ".log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
-        var path = this.dir + "/" + this.name + "/river.log"
-        fs.appendFile(path, message, function (err) {
-            if (err) throw err;
-        })
     };
-    debug(message, filename = "", comment = "") {
-        level = "DEBUG"
+
+    info(message, filename = "", comment = "") {
+        var path = this.dir + "/INFO.log"
+        var level = "INFO"
         var dt = new Date().toISOString();
         var entry = {
             "level": level,
@@ -104,20 +88,42 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             console.log(message);
         }
-        this.debug.push(entry);
+        this.infoRiver.push(entry);
         this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "DEBUG.log"
+        fs.appendFile(path, message, function (err) {
+            if (err) throw err;
+        });
+        writeRiver(this.dir, message);
+    };
+
+    debug(message, filename = "", comment = "") {
+        var level = "DEBUG"
+        var dt = new Date().toISOString();
+        var entry = {
+            "level": level,
+            "message": message,
+            "comment": comment,
+            "path": filename,
+            "time": dt
+        };
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
+        if (this.verbose) {
+            console.log(message);
+        }
+        this.debugRiver.push(entry);
+        this.river.push(entry);
+        var path = this.dir + "/" + "DEBUG.log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
         writeRiver(this.dir, message);
     };
     warn(message, filename = "", comment = "") {
-        level = "WARN"
+        var level = "WARN"
         var dt = new Date().toISOString();
         var entry = {
             "level": level,
@@ -126,20 +132,20 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             console.log(message);
         }
-        this.warn.push(entry);
+        this.warnRiver.push(entry);
         this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "WARN.log"
+        var path = this.dir + "/" + "WARN.log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
         writeRiver(this.dir, message);
     };
     error(message, filename = "", comment = "") {
-        level = "ERROR"
+        var level = "ERROR"
         var dt = new Date().toISOString();
         var entry = {
             "level": level,
@@ -148,20 +154,20 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             console.log(message);
         }
-        this.error.push(entry);
+        this.errorRiver.push(entry);
         this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "ERROR.log"
+        var path = this.dir + "/"  + "ERROR.log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
         writeRiver(this.dir, message);
     };
     critical(message, filename = "", comment = "") {
-        level = "CRITICAL"
+        var level = "CRITICAL"
         var dt = new Date().toISOString();
         var entry = {
             "level": level,
@@ -170,20 +176,20 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             console.log(message);
         }
-        this.critical.push(entry);
+        this.criticalRiver.push(entry);
         this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "CRITICAL.log"
+        var path = this.dir + "/"  + "CRITICAL.log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
         writeRiver(this.dir, message);
     };
     fatal(message, filename = "", comment = "") {
-        level = "FATAL"
+        var level = "FATAL"
         var dt = new Date().toISOString();
         var entry = {
             "level": level,
@@ -192,13 +198,13 @@ class lumberjack {
             "path": filename,
             "time": dt
         };
-        var message = dt + ' ' + level + ": " + message + " " + filename + ' ' + comment + "\r\n";
+        var message = dt + '   ' + this.name + '   ' + level + ":      " + message + "   " + filename + '     ' + comment + "\r\n";
         if (this.verbose) {
             console.log(message);
         }
-        this.fatal.push(entry);
+        this.fatalRiver.push(entry);
         this.river.push(entry);
-        var path = this.dir + "/" + this.name + "/" + "FATAL.log"
+        var path = this.dir + "/"  + "FATAL.log"
         fs.appendFile(path, message, function (err) {
             if (err) throw err;
         });
--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 const { lumberjack } = require("./index");
+var fs = require('fs');
 
-var lumber = new lumberjack("w", "w", "LOG", true);
-lumber.log("hello");
-lumber.log("hello", "FATAL");
-lumber.info("hello");
-lumber.warn("hello");
-lumber.error("hello");
-lumber.debug("hello");
-lumber.critical("hello");
-lumber.fatal("hello");
+console.log("INITIALISING TEST")
+try {
+    var lumber = new lumberjack("name", "path", "LOG", true);
+    lumber.log("Sucess!");
+    lumber.log("Sucess!", "FATAL");
+    lumber.info("Sucess!");
+    lumber.warn("Sucess!");
+    lumber.error("Sucess!");
+    lumber.debug("Sucess!");
+    lumber.critical("Sucess!");
+    lumber.fatal("Sucess!");
+    fs.rmSync("./path", { recursive: true, force: true });
+} catch(err) {
+    console.error("TEST FAILED")
+    console.error(err)
+} finally {
+    console.log("!!TEST SUCESS!!")
+};
